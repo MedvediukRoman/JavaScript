ded48db422f2d24f01fe15f887e55d7808b9ef62
MedvediukRoman <roma.medvedyuck@gmail.com>
2025-09-30 22:01:29 +0300

    зробив перше дз

---
+++ Homework1/task1-1/hw1-1.js
@@ -0,0 +1,26 @@
+let q='hello'
+let w='owu'
+let e='com'
+let r='ua'
+let t=1
+let y=10
+let u=-999
+let i=123
+let o=3.14
+let p=2.7
+let a=16
+let s=true
+let d=false
+console.log(q)
+console.log(w)
+console.log(e)
+console.log(r)
+console.log(t)
+console.log(y)
+console.log(u)
+console.log(i)
+console.log(o)
+console.log(p)
+console.log(a)
+console.log(s)
+console.log(d)
---
+++ Homework1/task1-2/hw1-2.js
@@ -0,0 +1,5 @@
+let firstName = 'Medvediuk '
+let middleName = 'Roman '
+let lastName = 'Andriyovych '
+let person=firstName+middleName+lastName
+console.log(person)

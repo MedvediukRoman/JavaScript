1f4a5234beaf63a10c2d35abe4c56568d7d8fcad
MedvediukRoman <roma.medvedyuck@gmail.com>
2026-03-10 21:27:39 +0200

    друге дз

---
+++ Homework2/main.js
@@ -0,0 +1,19 @@
+console.log(coursesAndDurationArray);
+if (coursesAndDurationArray[0].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[1].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[2].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[3].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[4].monthDuration >= 5) {
+    console.log('Супер')
+}
+if (coursesAndDurationArray[5].monthDuration >= 5) {
+    console.log('Супер')
+}
